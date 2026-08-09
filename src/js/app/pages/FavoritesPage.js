4e4e02ae663e677bedbe5274f36128d2b4f53c53
George Ruinelli <george@ruinelli.ch>
2026-08-09 02:38:32 +0200

    Adjust vertical positioning of favorite list items for better alignment
    - Move border up 1px (y+9 to y+8) and increase height by 2px (26 to 28)
    - Move title up 4px (y to y-4)
    - Move subtitle down 1px (y+19 to y+20)
    - Update scroll offset calculations to match new positions

--- src/js/app/pages/FavoritesPage.js
+++ src/js/app/pages/FavoritesPage.js
@@ -228,8 +228,8 @@ class FavoritesPage extends BasePage {
     var color = this.appState.favoriteEntityStore.getColor(item.id) || 'blue';
 
     var border = new UI.Rect({
-      position: new UI.Vector2(2, y + 9),
-      size: new UI.Vector2(26, 26),
+      position: new UI.Vector2(2, y + 8),
+      size: new UI.Vector2(26, 28),
       backgroundColor: color
     });
     win.add(border);
@@ -248,7 +248,7 @@ class FavoritesPage extends BasePage {
 
     var title = new UI.Text({
       text: item.title || '',
-      position: new UI.Vector2(32, y),
+      position: new UI.Vector2(32, y - 4),
       size: new UI.Vector2(168, 26),
       color: textColor,
       font: 'gothic_24_bold',
@@ -259,7 +259,7 @@ class FavoritesPage extends BasePage {
 
     var subtitle = new UI.Text({
       text: item.subtitle,
-      position: new UI.Vector2(32, y + 19),
+      position: new UI.Vector2(32, y + 20),
       size: new UI.Vector2(168, 18),
       color: textColor,
       font: 'gothic_18',
@@ -330,10 +330,10 @@ class FavoritesPage extends BasePage {
       var el = this._elements[i];
       if (!el) { continue; }
       el.highlight.position(new UI.Vector2(0, baseY - offset));
-      el.border.position(new UI.Vector2(2, baseY + 9 - offset));
+      el.border.position(new UI.Vector2(2, baseY + 8 - offset));
       if (el.icon) { el.icon.position(new UI.Vector2(3, baseY + 10 - offset)); }
-      el.title.position(new UI.Vector2(32, baseY - offset));
-      el.subtitle.position(new UI.Vector2(32, baseY + 19 - offset));
+      el.title.position(new UI.Vector2(32, baseY - 4 - offset));
+      el.subtitle.position(new UI.Vector2(32, baseY + 20 - offset));
     }
   }
 
