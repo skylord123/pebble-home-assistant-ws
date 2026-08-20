67a54b7d9446c3dc97c54b65cf2a720bbfa22e96
Skylar Sadlier <skylord123@gmail.com>
2026-08-19 22:01:28 -0600

    Bump to version 3.0

--- src/js/app/Constants.js
+++ src/js/app/Constants.js
@@ -5,7 +5,7 @@ const Feature = require('platform/feature');
 
 const Constants = {
     // App versioning
-    appVersion: '2.1',
+    appVersion: '3.0',
     confVersion: '1.2',
     configPageUrl: 'https://skylord123.github.io/pebble-home-assistant-ws/config/v1.3.html',
 
