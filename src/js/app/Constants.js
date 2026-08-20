e34107a541143815fd258d93db02dcbe5ff78222
Skylar Sadlier <skylord123@gmail.com>
2026-08-20 16:29:54 -0600

    Give v3.0 its own config page
    The touch settings added during v3.0 went into v1.3.html, which is the page 2.3
    shipped with, so anyone still on 2.3 was being offered switches for features
    their build does not have.

--- src/js/app/Constants.js
+++ src/js/app/Constants.js
@@ -7,7 +7,7 @@ const Constants = {
     // App versioning
     appVersion: '3.0',
     confVersion: '1.2',
-    configPageUrl: 'https://skylord123.github.io/pebble-home-assistant-ws/config/v1.3.html',
+    configPageUrl: 'https://skylord123.github.io/pebble-home-assistant-ws/config/v1.4.html',
 
     // Debug settings
     debugMode: true,
